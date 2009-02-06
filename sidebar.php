8ae8f68fd3c32416ec44a2693e9878382d327d61
Neil Crosby <neil@thetenwordreview.com>
2009-02-06 18:09:32 +0000

    Standardising the widgets on the static sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,14 +5,17 @@
     <?php
 
     $widget_setup = array(
-        'before_widget' => '<li>',
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
         'after_widget' => '</li>',
         'before_title' => '<h2>',
         'after_title' => '</h2>',
         );
+    $options = $widget_setup;
 
-    tct_widget_rss($widget_setup);
-    tct_widget_search($widget_setup);
+	$options['before_widget'] = sprintf($widget_setup['before_widget'], '', 'tct_widget_rss');
+    tct_widget_rss($options);
+	$options['before_widget'] = sprintf($widget_setup['before_widget'], '', 'tct_widget_search');
+    tct_widget_search($options);
 
     ?>
 
