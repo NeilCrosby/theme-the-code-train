a8741ade0772e7432df1f3b14e0885e7d7b1174b
Neil Crosby <neil@thetenwordreview.com>
2009-02-10 19:36:32 +0000

    MOAR Re-indenting

--- single.php
+++ single.php
@@ -1,30 +1,39 @@
 <?php get_header(); ?>
-<div id="yui-main"><div class="yui-b"> 
+<div id="yui-main">
+    <div class="yui-b"> 
 
-<div class="entry">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-<p>Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>. Filed under <strong><?php the_category(', ') ?></strong>.</p>
-<div class="articles">
-<?php the_content('Read the rest of this entry &raquo;'); ?>
-<p class="showtags"><?php if (function_exists('the_tags')) the_tags(__('Tags: ','ml'), ', ', '<br>'); ?></p>
-<div style="clear: both;"> </div>
-</div>
-    <!-- end content -->
-    <?php comments_template(); ?>
-        <?php endwhile; ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            
+        <div class="entry">
 
-<?php else : ?>
+            <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+            <p>
+                Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>. 
+                Filed under <strong><?php the_category(', ') ?></strong>.
+            </p>
+            <div class="articles">
+                <?php the_content('Read the rest of this entry &raquo;'); ?>
+                <p class="showtags">
+                    <?php if (function_exists('the_tags')) the_tags(__('Tags: ','ml'), ', ', '<br>'); ?>
+                </p>
+                <div style="clear: both;"> </div>
+            </div>
+
+            <?php comments_template(); ?>
+        
+        </div>
+        
+    <?php endwhile; ?>
+        
+    <?php else : ?>
 
         <h2>Not Found</h2>
         <p>Sorry, but you are looking for something that isn't here.</p>
 
     <?php endif; ?>
     
+    </div>
 </div>
-</div></div>
 
 <?php get_sidebar(); ?>
 
