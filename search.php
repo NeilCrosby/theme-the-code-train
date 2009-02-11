6a4fd3d7f4b36f2d1c2b9edcfd845cd91ec775c0
Neil Crosby <neil@thetenwordreview.com>
2009-02-11 18:34:41 +0000

    MOAR Re-indenting

--- search.php
+++ search.php
@@ -1,39 +1,51 @@
 <?php get_header(); ?>
-<div id="yui-main"><div class="yui-b"> 
+<div id="yui-main">
+    <div class="yui-b"> 
 
-<?php if (have_posts()) : ?>
+    <?php if (have_posts()) : ?>
 
-    <h2>Search Results</h2>
-    <p>You searched for "<?php the_search_query() ?>". This is what we found:</p>
+        <h2>Search Results</h2>
+        <p>
+            You searched for "<?php the_search_query() ?>". This is what we found:
+        </p>
 
-    <ol class="entries">
-    <?php while (have_posts()) : the_post(); ?>
-        <li>
-            <h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-            <p>
-                Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>. 
-                Filed under <strong><?php the_category(', ') ?></strong>.
-            </p>
-            <p class="showtags"><?php if (function_exists('the_tags')) the_tags(__('Tags: ','ml'), ', ', ''); ?>.</p>
-            <div class="count"><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></div>
-        </li>
-    <?php endwhile; ?>
-    </ol>
+        <ol class="entries">
+    
+        <?php while (have_posts()) : the_post(); ?>
+        
+            <li>
+                <h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                <p>
+                    Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>. 
+                    Filed under <strong><?php the_category(', ') ?></strong>.
+                </p>
+                <p class="showtags">
+                    <?php if (function_exists('the_tags')) the_tags(__('Tags: ','ml'), ', ', ''); ?>.
+                </p>
+                <div class="count">
+                    <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
+                </div>
+            </li>
+    
+        <?php endwhile; ?>
+    
+        </ol>
 
-    <ul class="nav-timeline">
-        <li class="prev"><?php next_posts_link('&laquo; Older Entries') ?></li>
-        <li class="next"><?php previous_posts_link('Newer Entries &raquo;') ?></li>
-    </ul>
+        <ul class="nav-timeline">
+            <li class="prev"><?php next_posts_link('&laquo; Older Entries') ?></li>
+            <li class="next"><?php previous_posts_link('Newer Entries &raquo;') ?></li>
+        </ul>
 
-<?php else : ?>
+    <?php else : ?>
 
-    <h2>Nothing Found</h2>
-    <p>Sorry, but you searched for something that isn't here.</p>
-    <P>Maybe you can search again?</p>
+        <h2>Nothing Found</h2>
+        <p>Sorry, but you searched for something that isn't here.</p>
+        <P>Maybe you can search again?</p>
 
-<?php endif; ?>
+    <?php endif; ?>
 
-</div></div>
+    </div>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
