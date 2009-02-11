4301b97f4c00f45f74aa50114ab68a914e06c12f
Neil Crosby <neil@thetenwordreview.com>
2009-02-11 18:57:56 +0000

    Entry-ise and re-indent page template

--- page.php
+++ page.php
@@ -1,25 +1,34 @@
 <?php get_header(); ?>
-<div id="yui-main"><div class="yui-b"> 
+<div id="yui-main">
+    <div class="yui-b"> 
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<h2>* <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<p>Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>.</p>
-<div class="articles">
-<?php the_content('Read the rest of this entry &raquo;'); ?><div style="clear: both;"> </div>
-</div>
-    <!-- end content -->
-    <?php comments_template(); ?>
-        <?php endwhile; ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            
+        <div class="entry">
+            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <p>
+                Posted on <?php the_time('F jS, Y') ?> by <?php the_author() ?>.
+            </p>
+            <div class="articles">
+                <?php the_content('Read the rest of this entry &raquo;'); ?>
+                <div style="clear: both;"> </div>
+            </div>
 
-<?php else : ?>
+            <?php comments_template(); ?>
+
+        </div>
+
+    <?php endwhile; ?>
+
+    <?php else : ?>
 
         <h2>Not Found</h2>
         <p>Sorry, but you are looking for something that isn't here.</p>
 
     <?php endif; ?>
-</div></div>
+
+    </div>
+</div>
 
 <?php get_sidebar(); ?>
 
