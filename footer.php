a0cfda37877f72c2ebf63f8abfe7a43dd6716082
Neil Crosby <neil@thetenwordreview.com>
2009-02-12 21:25:34 +0000

    Added github link to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     </div>
 
     <div id="ft">
-        Theme by <a href="http://helemaalniet.nl">Helemaal Niet!</a>, 
+        <a href="http://github.com/NeilCrosby/theme-the-code-train/tree">TheCodeTrain Theme</a> by <a href="http://neilcrosby.com/vcard">Neil Crosby</a>, 
         Powered by <a href="http://wordpress.org">WordPress</a>
     </div>
 </div>
