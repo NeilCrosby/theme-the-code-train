8991fdc60f3da98fb4fd8b5ccaaae593d5b509e1
Neil Crosby <neil@neilcrosby.com>
2010-05-27 22:50:12 +0100

    Added support for series of articles

--- single.php
+++ single.php
@@ -12,6 +12,42 @@
                 Filed under <strong><?php the_category(', ') ?></strong>.
             </p>
             <div class="articles">
+
+
+                <?php
+                $permalink = get_permalink();
+                $seriesTag = get_post_meta($post->ID, 'series_tag', true);
+                if ($seriesTag) :
+                    $seriesPosts = get_posts(array(
+                        'tag'     => $seriesTag,
+                        'orderby' => 'date',
+                        'order'   => 'ASC'
+                    ));
+                    if ($seriesPosts) :
+                        $tempPost = $post;
+                ?>
+
+                <div class="series">
+                    <h2>This post is part of a series:</h2>
+                    <ol>
+                    <?php foreach($seriesPosts as $post): ?>
+                        <li>
+                            <?php if ($permalink === get_permalink()) { 
+                                the_title();
+                            } else { ?>
+                                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                            <?php } ?>
+                        </li>
+                    <?php endforeach; ?>
+                    </ol>
+                </div>
+
+                <?php
+                        $post = $tempPost;
+                    endif;
+                endif;
+                ?>
+
                 <?php the_content('Read the rest of this entry &raquo;'); ?>
                 <p class="showtags">
                     <?php if (function_exists('the_tags')) the_tags(__('Tags: ','ml'), ', ', '<br>'); ?>
